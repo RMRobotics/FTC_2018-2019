3ac711e8cb3d9f7b4db92c7114bd1dd047a18bce
Angela Mu <angelamu2012@gmail.com>
2018-12-11 17:45:44 -0500

    updated autoCrater and autoDepot w/ field dimensions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/armisticeAuto/AutoCrater.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/armisticeAuto/AutoCrater.java
@@ -24,12 +24,12 @@ public class AutoCrater extends armisticeAutoSuper{
 
         //Get off lander
 
-        //Move forward to see Qube
-        moveEncoders(10, 1);
-
         //See Qube
         detector = new GoldAlignDetector();
         DogeCVYellowDetector(detector);
+
+        //Move forward to see Qube
+        moveEncoders(10, 1);
 
         //Vars
         int count = 0;
@@ -39,43 +39,79 @@ public class AutoCrater extends armisticeAutoSuper{
         ElapsedTime timer = new ElapsedTime();
         timer.reset();
 
-
-        while (detector.isFound() == false && timer.seconds() < 5){
-            strafeEncoders(3,direction);
-            count++;
-
-            if (change%2 == 1) {
-                totalDistance--;
-            }
-            else {
-                totalDistance++;
-            }
-
-            if (count > 5) {
-                strafeEncoders(14.5, -direction);
-                count = 0;
-                totalDistance = 0;
-                direction = -direction;
-                change++;
+        if (detector.getAligned().equals(Direction.CENTER)){
+            moveEncoders(2, 1);
+            //push mineral w/ arm
+        }
+        else if (detector.getAligned().equals(Direction.LEFT)){
+            while(detector.isFound()==false){
+                strafeEncoders(2, -1, .25);
             }
         }
-        if (detector.isFound() == true){
-            position = detector.getAligned();
-            strafeEncoders(20, 1);
+        else if (detector.getAligned().equals(Direction.RIGHT)){
+            while(detector.isFound()==false){
+                strafeEncoders(2, 1, .25);
+            }
         }
         else
         {
-            if (totalDistance < 0) {
-                totalDistance = -totalDistance + 20;
-            }
-            strafeEncoders(totalDistance, 1);
+            moveEncoders(2, -1);
         }
-        moveEncoders(5, 1); //knock off yellow mineral
+
+        //Detect Qube
+//        while (detector.isFound() == false && timer.seconds() < 5){
+//            strafeEncoders(3,direction,.25);
+//            count++;
+//
+//            if (change%2 == 1) {
+//                totalDistance--;
+//            }
+//            else {
+//                totalDistance++;
+//            }
+//
+//            if (count > 5) {
+//                strafeEncoders(14.5, -direction,.25);
+//                count = 0;
+//                totalDistance = 0;
+//                direction = -direction;
+//                change++;
+//            }
+//        }
+//        if (detector.isFound() == true){
+//            position = detector.getAligned();
+//            strafeEncoders(20, 1);
+//        }
+//        else
+//        {
+//            if (totalDistance < 0) {
+//                totalDistance = -totalDistance + 20;
+//            }
+//            strafeEncoders(totalDistance, 1);
+//        }
+
+        //knock off yellow mineral
+        moveEncoders(5, 1);
+
+        //go back to initial pos and turn
         moveEncoders(5, -1);
-        imuTurn(45, 0.4);
-        moveEncoders(20, 1);
+        imuTurn(90, 0.4);
+
+        //move to turn point and turn
+        moveEncoders(55, 1);
+        imuTurn(45,0.4);
+
+        //go to home depot
+        moveEncoders(32, 1);
+
         //drop off flag
+
+        //turn arouuuuuuund every now and then i get a little bit lonely
         imuTurn(180, 0.4);
-        moveEncoders(30, 1);
+
+        //travel from depot to crater
+        moveEncoders(69, 1);
+
+        //drop arm in crater
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/armisticeAuto/AutoDepot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/armisticeAuto/AutoDepot.java
@@ -39,43 +39,78 @@ public class AutoDepot extends armisticeAutoSuper{
         ElapsedTime timer = new ElapsedTime();
         timer.reset();
 
-
-        while (detector.isFound() == false && timer.seconds() < 5){
-            strafeEncoders(3,direction, .25);
-            count++;
-
-            if (change%2 == 1) {
-                totalDistance--;
-            }
-            else {
-                totalDistance++;
-            }
-
-            if (count > 5) {
-                strafeEncoders(14.5, -direction, .25);
-                count = 0;
-                totalDistance = 0;
-                direction = -direction;
-                change++;
+        if (detector.getAligned().equals(Direction.CENTER)){
+            moveEncoders(2, 1);
+            //push mineral w/ arm
+        }
+        else if (detector.getAligned().equals(Direction.LEFT)){
+            while(detector.isFound()==false){
+                strafeEncoders(2, -1, .25);
             }
         }
-        if (detector.isFound() == true){
-            position = detector.getAligned();
-            strafeEncoders(20, 1, .25);
+        else if (detector.getAligned().equals(Direction.RIGHT)){
+            while(detector.isFound()==false){
+                strafeEncoders(2, 1, .25);
+            }
         }
         else
         {
-            if (totalDistance < 0) {
-                totalDistance = -totalDistance + 20;
-            }
-            strafeEncoders(totalDistance, 1, .25);
+            moveEncoders(2, -1);
         }
-        moveEncoders(5, 1); //knock off yellow mineral
+
+//        while (detector.isFound() == false && timer.seconds() < 5){
+//            strafeEncoders(3,direction, .25);
+//            count++;
+//
+//            if (change%2 == 1) {
+//                totalDistance--;
+//            }
+//            else {
+//                totalDistance++;
+//            }
+//
+//            if (count > 5) {
+//                strafeEncoders(14.5, -direction, .25);
+//                count = 0;
+//                totalDistance = 0;
+//                direction = -direction;
+//                change++;
+//            }
+//        }
+//        if (detector.isFound() == true){
+//            position = detector.getAligned();
+//            strafeEncoders(20, 1, .25);
+//        }
+//        else
+//        {
+//            if (totalDistance < 0) {
+//                totalDistance = -totalDistance + 20;
+//            }
+//            strafeEncoders(totalDistance, 1, .25);
+//        }
+
+
+        //knock off yellow mineral
+        moveEncoders(5, 1);
+
+        //go back to initial pos and turn
         moveEncoders(5, -1);
-        imuTurn(45, 0.4);
-        moveEncoders(20, 1);
-        //drop off flag
+        imuTurn(90, 0.4);
+
+        //move to turn point and turn
+        moveEncoders(55, 1);
+        imuTurn(135,0.4);
+
+        //move to home depot and drop flag
+        moveEncoders(60,1);
+
+        //turn arouuuuuuund every now and then i get a little bit lonely
         imuTurn(180, 0.4);
-        moveEncoders(30, 1);
+
+        //travel from depot to crater
+        moveEncoders(69, 1);
+
+        //drop arm in crater
+
     }
 }
