3feb2f7af5a90dee42c0b6afff1971e5628a29d3
nachado12 <32498212+nachado12@users.noreply.github.com>
2018-12-13 16:00:09 -0500

    added class to test encoders

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/armisticeAuto/distanceTest.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.armisticeAuto;
+
+import com.disnodeteam.dogecv.detectors.roverrukus.Direction;
+import com.disnodeteam.dogecv.detectors.roverrukus.GoldAlignDetector;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+/**
+ * Created by Neal on 12/11/2018.
+ */
+
+@Autonomous(name = "distanceTest")
+public class distanceTest extends armisticeAutoSuper{
+
+    private GoldAlignDetector detector;
+    private Direction position = Direction.UNKNOWN;
+
+
+    public void runOpMode(){
+
+        //initialization
+        initialize(DcMotor.RunMode.RUN_USING_ENCODER);
+        waitForStart();
+
+        //Get off lander
+
+        //Move forward one foot
+        moveEncoders(12, 1);
+
+        sleep(10000);
+
+        moveEncoders(12, 1);
+
+        sleep(10000);
+
+        moveEncoders(12, 1);
+
+        sleep(10000);
+
+
+
+    }
+}
